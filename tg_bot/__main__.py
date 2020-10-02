ee8aa3f515fdb58b07535615ed1e4fcfa8532253
Jp-31 <72206460+Jp-31@users.noreply.github.com>
2020-10-02 19:40:25 +0800

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,31 +18,25 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+Kumusta ka {}
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
+Ako ay nasa pangangalaga ni [JP](tg://user?id={}).
 
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
+Para sa karagdagang imposmasyon,mangyaring gamitin ang  /help
 
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
-you can create clone same like me..
-
-For more commands click /help...
-
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
+** MARAMING SALAMAT **
 
 """
 
 HELP_STRINGS = """
 
-Hello! my name *{}*.
+Hello! Narito ang ilan sa mga commands na maaaring magamit.
 
 *Main* available commands:
  - /start: Start the bot...
- - /help: help....
- - /donate: To find out more about donating!
+ - /help: help
  - /settings:
-   - in PM: To find out what SETTINGS you have set....
+   - in PM: To find out what SETTINGS you have set
    - in a group:
 
 {}
